871dd3d7b8477bde8c2e57d4e443c665f0fca5db
izamiktsr <mazibackup2@gmail.com>
2024-10-22 15:24:58 +0700

    new version

---
+++ PersegiAngka518.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class PersegiAngka518 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan nilai N (minimal 3): ");
+        int N = scanner.nextInt();
+
+        if (N < 3) {
+            System.out.println("Nilai N harus minimal 3.");
+        } else {
+            // Menampilkan angka 5 di baris pertama dan terakhir
+            for (int i = 1; i <= N; i++) {
+                System.out.print("5 ");
+            }
+            System.out.println();
+
+            // Menampilkan angka 5 di kolom pertama dan terakhir
+            for (int i = 1; i <= N - 2; i++) {
+                System.out.print("5 ");
+                for (int j = 1; j <= N - 2; j++) {
+                    System.out.print("  ");
+                }
+                System.out.println("5");
+            }
+
+            // Menampilkan angka 5 di baris terakhir
+            for (int i = 1; i <= N; i++) {
+                System.out.print("5 ");
+            }
+            System.out.println();
+        }
+    }
+}
